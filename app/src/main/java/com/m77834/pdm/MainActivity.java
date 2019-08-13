61712d2e0d282bbdb4cb4f8820997ec299e62937
m77834 <spiegel@mx2.unisc.br>
2019-08-13 20:00:56 -0300

    Criado nova activity

--- app/src/main/java/com/m77834/pdm/MainActivity.java
+++ app/src/main/java/com/m77834/pdm/MainActivity.java
@@ -2,12 +2,22 @@ package com.m77834.pdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String Tag = "APP";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(Tag,"onStart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(Tag,"onStart");
     }
 }
