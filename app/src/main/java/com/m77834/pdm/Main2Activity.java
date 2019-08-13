dc1fcfaf92a32cd2748cb096cbcde3d32ad4e9ab
m77834 <spiegel@mx2.unisc.br>
2019-08-13 20:43:03 -0300

    change

--- app/src/main/java/com/m77834/pdm/Main2Activity.java
+++ app/src/main/java/com/m77834/pdm/Main2Activity.java
@@ -2,18 +2,12 @@ package com.m77834.pdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void aula2Click(View view){
         setContentView(R.layout.activity_main2);
     }
 }
